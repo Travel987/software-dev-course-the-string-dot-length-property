f713b03a5081a8fad428a6ca45c2c46ee61726e3
Travel987 <brianapowers78@gmail.com>
2025-09-07 12:31:12 -0500

    Complete .length practice

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -20,11 +20,9 @@ let stringTwo = "JavaScript";
 let stringThree = "";
 
 // Your task: Assign the length of each string to the corresponding variable
-let lengthOne;   // Length of stringOne
-let lengthTwo;   // Length of stringTwo
-let lengthThree; // Length of stringThree
-
-// Your code here
+let lengthOne = stringOne.length;    // Length of "Coding Bootcamp"
+let lengthTwo = stringTwo.length;    // Length of "JavaScript"
+let lengthThree = stringThree.length; // Length of ""
 
 
 
